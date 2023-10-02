f33a6430f8c4549bf1ba51b94280f64ae5ebc771
Miguel Vázquez <miguelvz26.mv@gmail.com>
2023-10-02 10:37:02 -0600

    accordion in side menu completed

--- resources/js/Components/MyComponents/SideNav.vue
+++ resources/js/Components/MyComponents/SideNav.vue
@@ -4,25 +4,20 @@
         <div class="bg-[#313131] h-full overflow-auto">
             <nav class="px-2 pt-32 text-white">
                 <template v-for="(menu, index) in menus" :key="index">
-                    <button @click="handleClickInMenu(index)" v-if="menu.show" :href="menu.route" :active="menu.active"
-                        :dropdown="menu.dropdown"
-                        class="w-full text-start px-2 mb-1 flex justify-between text-xs rounded-md py-1"
-                        :class="menu.active ? 'font-bold text-[#FD8827]' : ''">
+                    <Accordion v-if="menu.options.length" :icon="menu.icon" :active="menu.active" :title="menu.label" :id="index">
+                        <div v-for="(option, index2) in menu.options" :key="index2">
+                            <button @click="goToRoute(option.route)" v-if="option.show" :active="option.active"
+                                class="w-full text-start pl-6 pr-2 mb-1 flex justify-between text-xs rounded-md py-1"
+                                :class="option.active ? 'bg-[#FD8827] text-white' : 'hover:text-[#FD8827]'">
+                                <p class="w-full truncate"> {{ option.label }}</p>
+                            </button>
+                        </div>
+                    </Accordion>
+                    <button v-else @click="goToRoute(menu.route)" v-if="menu.show" :active="menu.active"
+                        class="w-full text-start px-2 mb-1 flex justify-between text-xs rounded-md py-1 hover:text-[#FD8827]"
+                        :class="menu.active ? 'bg-[#FD8827] text-white' : ''">
                         <p class="w-full truncate"><span v-html="menu.icon"></span> {{ menu.label }}</p>
-                        <span v-if="menu.dropdown"><i class="fa-solid fa-angle-down"></i></span>
                     </button>
-                    <transition name="collapse-transition">
-                        <ul v-if="collapsedMenu == index">
-                            <template v-for="(option, index2) in menu.options" :key="index2">
-                                <button @click="goToRoute(option.route)" v-if="option.show" :href="option.route"
-                                    :active="option.active" :dropdown="option.dropdown"
-                                    class="w-full px-2 mb-1 flex justify-between text-xs rounded-md py-1"
-                                    :class="option.active ? 'bg-[#FD8827] text-white' : ''">
-                                    <p class="w-full truncate"> {{ option.label }}</p>
-                                </button>
-                            </template>
-                        </ul>
-                    </transition>
                 </template>
             </nav>
         </div>
@@ -30,6 +25,7 @@
 </template>
 
 <script>
+import Accordion from './Accordion.vue';
 
 export default {
     data() {
@@ -39,31 +35,30 @@ export default {
                 {
                     label: 'CRM',
                     icon: '<i class="fa-solid fa-chart-line mr-1"></i>',
-                    active: true,//route().current('crm.*'),
+                    active: route().current('crm.*'),
                     options: [
                         {
                             label: 'Inicio',
                             route: route('crm.dashboard'),
                             // show: this.$page.props.auth.user.permissions.includes('Inicio crm'),
-                            active: true,//route().current('crm.*'),
+                            active: route().current('crm.dashboard'),
                             show: true,
                         },
                         {
                             label: 'Oportunidades',
                             route: route('crm.opportunities.index'),
                             // show: this.$page.props.auth.user.permissions.includes('Ver oportunidades'),
-                            active: false,//route().current('crm.*'),
+                            active: route().current('crm.opportunities.index'),
                             show: true,
                         },
                         {
                             label: 'Clientes',
                             route: route('crm.customers.index'),
                             // show: this.$page.props.auth.user.permissions.includes('Ver clientes'),
-                            active: false,//route().current('crm.*'),
+                            active: route().current('crm.customers.index'),
                             show: true,
                         },
                     ],
-                    dropdown: true,
                     // show: this.$page.props.auth.user.permissions.includes('Ver cotizaciones') ||
                     //     this.$page.props.auth.user.permissions.includes('Ver clientes')
                     show: true,
@@ -78,18 +73,17 @@ export default {
                             label: 'Inicio',
                             route: route('pms.dashboard'),
                             // show: this.$page.props.auth.user.permissions.includes('Inicio pms'),
-                            active: true,//route().current('crm.*'),
+                            active: route().current('pms.dashboard'),
                             show: true,
                         },
                         {
                             label: 'Proyectos',
                             route: route('pms.projects.index'),
                             // show: this.$page.props.auth.user.permissions.includes('Ver proyectos'),
-                            active: true,//route().current('crm.*'),
+                            active: route().current('pms.projects.index'),
                             show: true,
                         },
                     ],
-                    dropdown: true,
                     // show: this.$page.props.auth.user.permissions.includes('Ver proyectos')
                     show: true,
                 },
@@ -98,7 +92,7 @@ export default {
                     icon: '<i class="fa-solid fa-user mr-1"></i>',
                     route: route('users.index'),
                     active: route().current('users.*'),
-                    dropdown: false,
+                    options: [],
                     // show: this.$page.props.auth.users.permissions.includes('Ver usuarios')
                     show: true,
                 },
@@ -107,7 +101,7 @@ export default {
                     icon: '<i class="fa-solid fa-gear mr-1"></i>',
                     route: route('settings.index'),
                     active: route().current('settings.*'),
-                    dropdown: false,
+                    options: [],
                     // show: this.$page.props.auth.user.permissions.includes('Ver configuraciones')
                     show: true,
                 },
@@ -115,6 +109,7 @@ export default {
         }
     },
     components: {
+        Accordion,
     },
     methods: {
         handleClickInMenu(index) {
